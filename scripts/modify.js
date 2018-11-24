8066d8f93c4efaea7b6b1003572e68f99c39ac33
Christopher Buteau <cbuteau@gmail.com>
2018-11-24 16:48:50 -0500

    first pass modification script

--- scripts/modify.js
+++ scripts/modify.js
@@ -6,6 +6,77 @@ var escodegen = require('escodegen');
 
 // http://www.mattzeunert.com/2013/12/30/parsing-and-modifying-Javascript-code-with-esprima-and-escodegen.html
 
+/*
+GOALS
+
+parse the define and the modules to load...
+get the basename of each and replace with the parameter passed in...
+
+like src/TroubleMaker
+to
+.. DS/WebJobs/Troublemaker.
+
+
+*/
+
+if (process.argv.length < 3) {
+  console.log('You need a parent path for the define paths');
+  return;
+}
+
+var replaceparent = process.argv[2];
+
+function findSection(struct, type, name) {
+  if (struct.type === type) {
+    if (struct.expression && struct.expression.callee && struct.expression.callee.name === name) {
+      return struct;
+    }
+  }
+
+  var keys = Object.keys(struct);
+  for (var i = 0; i < keys.length; i++) {
+    var sub = struct[keys[i]];
+    if (Array.isArray(sub)) {
+      for (var j = 0; i < sub.length; i++) {
+        var arrayChild = sub[i];
+        var result = findSection(arrayChild, type, name);
+        if (result) {
+          return result;
+        }
+      }
+    } else {
+      // if (sub.type === type) {
+      //   if (sub.callee && sub.callee.name === name) {
+      //     return sub;
+      //   }
+      // }
+    }
+  }
+}
+
+function remapDefinePath(definepath, newparentpath) {
+  var parent = path.dirname(definepath);
+  var basename = path.basename(definepath);
+
+  return path.posix.join(newparentpath, basename);
+}
+
+function remapDefine(defineSegment, newparentpath) {
+  for (var i = 0; i < defineSegment.expression.arguments.length; i++) {
+    var arg = defineSegment.expression.arguments[i];
+    if (arg.type === 'Literal') {
+      arg.value = remapDefinePath(arg.value, newparentpath);
+    }
+    if (arg.type === 'ArrayExpression') {
+      for (var j = 0; j < arg.elements.length; j++) {
+        var elem = arg.elements[j];
+        elem.value = remapDefinePath(elem.value, newparentpath);
+      }
+    }
+  }
+}
+
+
 var srcfiles = fs.readdirSync('./src');
 for (var i = 0; i < srcfiles.length; i++) {
   var src = srcfiles[i];
@@ -13,7 +84,11 @@ for (var i = 0; i < srcfiles.length; i++) {
   console.log(full);
   var data = fs.readFileSync(full, 'utf8');
   var parsed = esprima.parse(data);
+  var thedefine = findSection(parsed, 'ExpressionStatement', 'define');
+  remapDefine(thedefine, replaceparent);
   var outpath = path.join(__dirname, '../out/', path.basename(src, '.js') + '.json');
+  var outpathjs = path.join(__dirname, '../out/', path.basename(src, '.js') + '.js');
   console.log(outpath);
   fs.writeFileSync(outpath, JSON.stringify(parsed, null, 2));
+  fs.writeFileSync(outpathjs, escodegen.generate(parsed));
 }
